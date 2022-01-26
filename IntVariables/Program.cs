3d8eec49e0b394ad4a9a92c90a9a5985c39240bc
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-26 10:45:11 -0600

    Understood the assignment

---
+++ IntVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// int hold whole numbers only//
+
+int age = 0;
+
+age = 43;
+
+int ageInTenYears = age + 10;
+//age = age + 10;
+//Two billion +/-
+//signed int32
+//unsigned - 4 billion
+// bit - 0 or 1
+//byte - 8 bits - 00000000
+// 1, 11, 111
+
+
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+// Don't divide ints - Console.WriteLine(age / 2);
+Console.ReadLine();
+
+
+
+
